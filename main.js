aca46d85f8fe59787bed17b75fc9bdcae7e86ebc
samlam369 <9115357+samlam369@users.noreply.github.com>
2025-05-04 23:26:02 +0800

    refactor: remove the fullscreen logic injection as it's no longer needed.

--- main.js
+++ main.js
@@ -32,7 +32,6 @@ function createWindow() {
   mainWindow.loadURL('https://keep.google.com/u/0/');
 
   mainWindow.webContents.on('did-finish-load', () => {
-    // injectFullScreenExtension(mainWindow.webContents); // Temporarily disabled for sideload experiment
   });
 
   mainWindow.on('close', (e) => {
@@ -48,20 +47,6 @@ function createWindow() {
   mainWindow.setMenuBarVisibility(false);
 }
 
-function injectFullScreenExtension(webContents) {
-  const fs = require('fs');
-  const path = require('path');
-  const jsPath = path.join(__dirname, 'injected', 'fullscreen.js');
-  const cssPath = path.join(__dirname, 'injected', 'fullscreen.css');
-  if (fs.existsSync(jsPath)) {
-    const script = fs.readFileSync(jsPath, 'utf8');
-    webContents.executeJavaScript(script);
-  }
-  if (fs.existsSync(cssPath)) {
-    const css = fs.readFileSync(cssPath, 'utf8');
-    webContents.insertCSS(css);
-  }
-}
 
 function getIconPath() {
   // Use a default icon if none exists
